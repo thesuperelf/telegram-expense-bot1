b98aab49cbc4b31768ebc63aa72d62ef7185bd21
thesuperelf <mihalych212121@gmail.com>
2026-04-22 08:06:09 +0300

    Add database middleware

---
+++ middlewares/db.py
@@ -0,0 +1,25 @@
+from collections.abc import Awaitable, Callable
+from typing import Any
+
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+
+
+class DbSessionMiddleware(BaseMiddleware):
+    def __init__(self, session_maker: async_sessionmaker[AsyncSession]) -> None:
+        self._session_maker = session_maker
+
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: dict[str, Any],
+    ) -> Any:
+        async with self._session_maker() as session:
+            try:
+                data["session"] = session
+                return await handler(event, data)
+            except Exception:
+                await session.rollback()
+                raise
